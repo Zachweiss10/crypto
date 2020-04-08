4c18631e365e3831c66a7fc17a0c0996d1a36850
zachvice <zachweiss@gmail.com>
2020-04-07 21:12:54 -0700

    fixed add...need parser

--- project.py
+++ project.py
@@ -115,19 +115,25 @@ def add(inputString):
     caseID = inputString[3]
     inputString = inputString[4:]
     numOfCaseItems = len(inputString)
-    #print(inputString)
     for i in range(0,numOfCaseItems):
         if i % 2 == 1:
             itemID.append(inputString[i])
         else:
             if inputString[i] != "-i":
                 dieWithError() 
-    print(len(itemID))
-    blockFile = open(BCHOC_FILE_PATH, 'ab')    #create new block
+    #need to hash parent
+    #create new block
+    blockFile = open(BCHOC_FILE_PATH, 'ab') 
     for j in range(0, len(itemID)):
-        packedData = Block(prevHash=bytes(0x00), state="CHECKEDIN", caseID=bytes(0x00), evidenceID= int(itemID[j]), dataLength=0, data="").packData()
+        currTime = datetime.datetime.now(datetime.timezone.utc)
+        timestamp = currTime.timestamp()
+        packedData = Block(prevHash=bytes(0x00), timestamp=timestamp, state="CHECKEDIN", caseID=bytes(0x00), evidenceID= int(itemID[j]), dataLength=0, data="").packData()
         blockFile.write(packedData)
         print("Added item:",end=" ")
+        print(itemID[j])
+        print("  Status: CHECKEDIN")
+        print("  Time of action: ",end="")
+        print(currTime)
 
     blockFile.close()
 
