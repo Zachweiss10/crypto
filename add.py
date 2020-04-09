8f686a535cbdf85eb1f8f2f2f00c5208b5a27399
zachvice <zachweiss@gmail.com>
2020-04-09 13:56:02 -0700

    fixed add.py

--- add.py
+++ add.py
@@ -9,23 +9,9 @@ from parse import *
 
 BCHOC_FILE_PATH = "./blocParty"
 
-def add(inputString):
-    #Store case id, and (multiple) itemId
-    #Ensure input errors end with a dieWithError()
-    itemID = []
-    if inputString[2] != "-c":
-        dieWithError()
-    caseID = inputString[3]
-    inputString = inputString[4:]
-    numOfCaseItems = len(inputString)
-    for i in range(0,numOfCaseItems):
-        if i % 2 == 1:
-            itemID.append(inputString[i])
-        else:
-            if inputString[i] != "-i":
-                dieWithError() 
+def add(caseId, itemID):
     #need to hash parent
-    parse()
+
 
 
     #check
@@ -35,7 +21,10 @@ def add(inputString):
     for j in range(0, len(itemID)):
         currTime = datetime.datetime.now(datetime.timezone.utc)
         timestamp = currTime.timestamp()
-        packedData = Block(prevHash=bytes(0x00), timestamp=timestamp, state="CHECKEDIN", caseID=bytes(0x00), evidenceID= int(itemID[j]), dataLength=0, data="").packData()
+        #print(type(val))
+        #print(itemID)
+        #print(type(itemID[j]))
+        packedData = Block(prevHash=bytes(0x00), timestamp=timestamp, state="CHECKEDIN", caseID=bytes(0x00), evidenceID= int(itemID[j][0]), dataLength=0, data="").packData()
         blockFile.write(packedData)
         print("Added item:",end=" ")
         print(itemID[j])
