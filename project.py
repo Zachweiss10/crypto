f5cd062a9a69b80f71f781334a24f89c6d97cdea
jtoews10 <37600125+jtoews10@users.noreply.github.com>
2020-04-09 15:27:27 -0700

    fix parse and add checkout to project.py

--- project.py
+++ project.py
@@ -13,78 +13,6 @@ from parse import *
 
 BCHOC_FILE_PATH = "./blocParty"
 
-class Block:
-
-    def __init__(self,
-                 prevHash=None,
-                 timestamp=None,
-                 caseID=None,
-                 evidenceID=None,
-                 state=None,
-                 dataLength=None,
-                 data=None ):
-        self.prevHash = prevHash
-        self.timestamp = timestamp
-        self.caseID = caseID
-        self.evidenceID = evidenceID
-        self.state = state
-        self.dataLength = dataLength
-        self.data = data
-        self._dataString = None
-        self._hash = None
-
-    def packData(self):
-        if ( self.timestamp == None ):
-            currTime = datetime.datetime.now(datetime.timezone.utc)
-            self.timestamp = currTime.timestamp()
-        if ( self.prevHash == None ):
-            print( "No hash provided, the data couldn't be packed")
-            return
-        if ( self.caseID == None ):
-            print( "No caseID provided, the data couldn't be packed")
-            return
-        if ( self.evidenceID == None ):
-            print( "No evidenceID provided, the data couldn't be packed")
-            return
-        if ( self.state == None ):
-            print( "No state provided, the data couldn't be packed")
-            return
-        if ( self.dataLength == None ):
-            print( "No dataLength provided, the data couldn't be packed")
-            return
-        if ( self.data == None and self.dataLength != 0):
-            print( "No data provided, the data couldn't be packed")
-            return
-        fmtString = "20s d 16s I 11s I {dataLength}s".format(dataLength=self.dataLength)
-        packedData = struct.pack(fmtString, self.prevHash, self.timestamp, self.caseID, self.evidenceID,
-                                 self.state.encode(), self.dataLength, self.data.encode())
-        self._dataString = packedData
-        self._hash = hashlib.sha1(packedData)
-        return packedData
-
-    def unpackData(self, data):
-        unpackedData = struct.unpack_from("20s d 16s I 11s I", data, 0)
-        self.prevHash = unpackedData[0]
-        self.timestamp = unpackedData[1]
-        self.caseID = unpackedData[2]
-        self.evidenceID = unpackedData[3]
-        self.state = (unpackedData[4]).decode()
-        self.dataLength = unpackedData[5]
-        unpackedData = struct.unpack_from("{dataLength}s".format(dataLength=self.dataLength), data, 68)
-        self.data = (unpackedData[0]).decode()
-
-        self._dataString = data[0:68 + self.dataLength]
-        self._hash = hashlib.sha1(self._dataString)
-
-    def getHash(self):
-        if self._hash != None:
-            return self._hash
-        if self._dataString != None:
-            return hashlib.sha1(self._dataString)
-
-        print("Get hash was called on a block without data! Call pack or unpack data to generate a hash")
-        return 0
-
 
 # Successful commands should exit with 0
 def dieWithSuccess():
@@ -126,26 +54,41 @@ def init():
 #add command created n numbers of items for a specific caseID
 
 
-def checkout(evidenceID):
+def checkout(evidenceIDList):
+    blockList = parse()
     maxTime = 0
     recentBlock = None
 
     #verify input
-    if evidenceID == None:
+    if evidenceIDList == None:
+        print("no evidence id provided!")
         dieWithError()
 
-    for block in blockList:
-        if evidenceID == block.evidenceID:
-            if block.timestamp > maxTime:
-                maxTime = max(maxTime, block.timestamp)
-                recentBlock = block
+    for evidenceID in evidenceIDList:
+        for block in blockList:
+            if evidenceID[0] == block.evidenceID:
+                if block.timestamp > maxTime:
+                    maxTime = max(maxTime, block.timestamp)
+                    recentBlock = block
 
-    #if the evidenceID doesn't exist
-    if recentBlock == None:
-        dieWithError()
+        #if the evidenceID doesn't exist
+        if recentBlock == None:
+            print("no block exists with that evidence id!")
+            dieWithError()
 
-    if recentBlock.state == "CHECKEDIN":
-        newBlock = Block()
+        tempState = "".join(e for e in recentBlock.state if e.isalnum())
+        if tempState == "CHECKEDIN":
+            pHash = blockList[-1].getHash()
+            newBlock = Block(prevHash=pHash, caseID=recentBlock.caseID, evidenceID=evidenceID[0], state="CHECKEDOUT", dataLength=0, data="" )
+            data = newBlock.packData()
+            blockFile = open(BCHOC_FILE_PATH, 'ab')
+            blockFile.write(data)
+            blockFile.close()
+            blockList.append(newBlock)
+        else:
+            print("block must be checkedin to be checked out!")
+            dieWithError()
+
 
 
 
